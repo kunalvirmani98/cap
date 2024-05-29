622f413e43a165844e7376bea80656498a10687f
kunal virmani <kunalvirmani98@gmail.com>
2024-05-29 21:41:30 +0530

    added react router and a basic route

--- src/index.js
+++ src/index.js
@@ -5,11 +5,22 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store/store';
 import { Provider } from 'react-redux';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+
+const router = createBrowserRouter([
+  {
+    path: "/dashboard",
+    element: <App />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>
 );
 
