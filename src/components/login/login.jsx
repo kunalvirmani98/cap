ea897e3e2bb20d531b1ac113b753671fd04221bb
kunal virmani <kunalvirmani98@gmail.com>
2024-06-08 15:02:19 +0530

    login component design changes

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import {useState} from "react";
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
-
-let validEmailRegEx = '';
+import { Box, Typography } from "@mui/material";
 
 export default function Login () {
     const [email, setEmail] = useState('');
@@ -22,14 +21,6 @@ export default function Login () {
     // Password does not match
     // Network connection error
 
-    function handleLogin (e) {
-
-        // Email Validity Check
-        // Email exist check in store
-        // Password match 
-        // Pass or Faild
-    }
-
     function handleEmail (e) {
         setEmail(e.target.value);
     }
@@ -38,15 +29,24 @@ export default function Login () {
         setPassword(e.target.value);
     }
 
+    function handleLogin (e) {
+        
+        // Email Validity Check
+        // Email exist check in store
+        // Password match 
+        // Pass or Failed
+    }
+
     return (
-        <form noValidate autoComplete="off">
-            <FormControl sx={{ width: '25ch' }} required>
-                <TextField id="email" label="Email" variant="standard" onChange={handleEmail} type='email' />
+        <Box autoComplete="off" component="form" className="login-window">
+            <p className="title">CAP</p>
+            <FormControl className="login-text-field">
+                <TextField fullWidth id="email" label="Email" variant="outlined" onChange={handleEmail} type='email' required />
             </FormControl>
-            <FormControl>
-                <TextField id="password" label="Password" type="password" variant="standard" />
+            <FormControl className="login-text-field">
+                <TextField fullWidth id="password" label="Password" type="password" variant="outlined" onChange={handlePassword} />
             </FormControl>
-            <button className="referral-button" style={{color: 'white'}} onClick={handleLogin}>LOG IN</button>
-        </form>
+            <button className="login-btn" onClick={handleLogin}>LOG IN</button>
+        </Box>
     )
 }
